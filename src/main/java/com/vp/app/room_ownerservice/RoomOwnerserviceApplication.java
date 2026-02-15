ad2641cc768e0d97349364b414ead2ef830bdaf7
vuthaphai <vuthaphai@gmail.com>
2026-02-15 12:29:56 +0700

    fist commit

---
+++ src/main/java/com/vp/app/room_ownerservice/RoomOwnerserviceApplication.java
@@ -0,0 +1,13 @@
+package com.vp.app.room_ownerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomOwnerserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomOwnerserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vp/app/room_ownerservice/RoomOwnerserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vp.app.room_ownerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomOwnerserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
